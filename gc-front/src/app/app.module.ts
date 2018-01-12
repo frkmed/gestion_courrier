407d2adbd9b69296e792163298202d2e2c46b10c
frkmed <medfrk3@gmail.com>
2018-01-12 20:23:36 +0000

    Ajouter un dossier pourorganiser les composants

--- gc-front/src/app/app.module.ts
+++ gc-front/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
