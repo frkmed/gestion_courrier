afc94f8821ff6d3ca59a6ee86b6534dae133a324
illoulyoussef <34659098+illoulyoussef@users.noreply.github.com>
2018-01-17 15:37:14 +0000

    page de recherche version 1

--- gc-front/src/app/component/recherche-courrier/recherche-courrier.component.ts
+++ gc-front/src/app/component/recherche-courrier/recherche-courrier.component.ts
@@ -7,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RechercheCourrierComponent implements OnInit {
 data=[
-  {'id':10,'type':'recherche','categorie':'prive'},
-  {'id':20,'type':'recherche','categorie':'prive'},
-  {'id':30,'type':'recherche','categorie':'prive'},
-  {'id':40,'type':'recherche','categorie':'prive'},
-  {'id':50,'type':'recherche','categorie':'prive'},
-  {'id':60,'type':'recherche','categorie':'prive'},
-  {'id':70,'type':'recherche','categorie':'prive'}
+  {'id':10,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'youssef','objet':'facture','destination':'service budget'},
+  {'id':20,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'mohamed','objet':'facture','destination':'service budget'},
+  {'id':30,'type':'recherche','categorie':'prive','date':'15/1/2018','correspondant': 'mehdi','objet':'facture','destination':'service budget'},
+  {'id':40,'type':'recherche','categorie':'public','date':'5/9/2017','correspondant': 'assia','objet':'facture','destination':'service budget'},
+  {'id':50,'type':'recherche','categorie':'prive','date':'23/8/2017','correspondant': 'hajhouj','objet':'facture','destination':'service budget'},
+  {'id':60,'type':'recherche','categorie':'prive','date':'23/6/2017','correspondant': 'mohamed','objet':'facture','destination':'service budget'},
+  {'id':70,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'youssef','objet':'facture','destination':'service budget'},
+  {'id':80,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'mohamed','objet':'facture','destination':'service budget'},
+  {'id':90,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'mehdi','objet':'facture','destination':'service budget'},
+  {'id':100,'type':'recherche','categorie':'public','date':'20/1/2018','correspondant': 'assia','objet':'facture','destination':'service budget'},
+  {'id':110,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'hajhouj','objet':'facture','destination':'service budget'},
+  {'id':120,'type':'recherche','categorie':'prive','date':'20/1/2018','correspondant': 'mohamed','objet':'facture','destination':'service budget'},
+  {'id':130,'type':'recherche','categorie':'prive','date':'27/1/2018','correspondant': 'youssef','objet':'facture','destination':'service budget'},
+  {'id':140,'type':'recherche','categorie':'prive','date':'18/9/2017','correspondant': 'youssef','objet':'facture','destination':'service budget'},
+  {'id':150,'type':'recherche','categorie':'prive','date':'08/8/2017','correspondant': 'mohamed','objet':'facture','destination':'service budget'},
+  {'id':160,'type':'recherche','categorie':'prive','date':'13/3/2017','correspondant': 'mehdi','objet':'facture','destination':'service budget'},
+  {'id':170,'type':'recherche','categorie':'public','date':'20/8/2017','correspondant': 'assia','objet':'facture','destination':'service budget'},
+  {'id':180,'type':'recherche','categorie':'prive','date':'1/1/2018','correspondant': 'hajhouj','objet':'facture','destination':'service budget'},
+  {'id':190,'type':'recherche','categorie':'prive','date':'20/6/2017','correspondant': 'mohamed','objet':'facture','destination':'service budget'},
+  {'id':200,'type':'recherche','categorie':'prive','date':'10/8/2017','correspondant': 'youssef','objet':'facture','destination':'service budget'},
+  {'id':210,'type':'recherche','categorie':'prive','date':'20/2/2017','correspondant': 'youssef','objet':'facture','destination':'service budget'}
 ];
   constructor() { }
 
